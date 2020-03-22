d3a4ed0a553acc5fbb3f58755e9412571783b9a4
HrX03 <dn.bianco03@gmail.com>
2020-03-22 17:21:39 +0100

    notes: Further progress on ui base

--- lib/routes/main_page.dart
+++ lib/routes/main_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
 import 'package:potato_notes/database/bloc/bloc_provider.dart';
 import 'package:potato_notes/database/bloc/notes_bloc.dart';
 import 'package:potato_notes/database/model/note.dart';
+import 'package:spicy_components/spicy_components.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -24,17 +27,31 @@ class _MainPageState extends State<MainPage> {
         stream: _notesBloc.notes,
         builder: (context, snapshot) {
           if((snapshot.data?.length ?? 0) != 0) {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
+            return StaggeredGridView.countBuilder(
+              crossAxisCount: 2,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(snapshot.data[index].content),
-                );
-              }
+                return Text(snapshot.data[index].content);
+              },
+              staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+              itemCount: snapshot.data.length,
             );
           } else return Text("bruh");
         },
-      )
+      ),
+      bottomNavigationBar: SpicyBottomBar(
+        leftItems: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            padding: EdgeInsets.all(0),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(OMIcons.edit),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
