608c6f5938e81114a5a4ca6d1db6737cac466d80
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2020-05-18 23:58:22 +0200

    notes: Match functions of note_controller with its interface
    The note_interface didnt really match what the API would ask for so the function signatures needed to be changed.

--- lib/internal/sync/interface/note_interface.dart
+++ lib/internal/sync/interface/note_interface.dart
@@ -1,9 +1,9 @@
 import 'package:potato_notes/data/database.dart';
 
 abstract class NoteInterface {
-  Note add(Note note);
-  Note update(Note note);
-  bool delete(int id);
+  Future<bool> add(Note note);
+  Future<bool> update(String id, Map<String, dynamic> noteDelta);
+  Future<bool> delete(String id);
   bool deleteAll();
   List<Note> list();
 }
