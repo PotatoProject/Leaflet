e79c961203a0da850826e402631b345d1b689e6b
HrX03 <dn.bianco03@gmail.com>
2020-06-03 09:15:17 +0200

    notes: Move note menu to Utils class

--- lib/internal/utils.dart
+++ lib/internal/utils.dart
@@ -1,5 +1,9 @@
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:potato_notes/data/database.dart';
+import 'package:potato_notes/internal/app_info.dart';
 import 'package:potato_notes/widget/pass_challenge.dart';
+import 'package:provider/provider.dart';
 
 class Utils {
   static Future<dynamic> showPassChallengeSheet(BuildContext context) async {
@@ -11,6 +15,64 @@ class Utils {
         onChallengeSuccess: () => Navigator.pop(context, true),
         onSave: null,
       ),
+    );
+  }
+
+  static Future<String> showNoteMenu(BuildContext context, [Note note]) async {
+    AppInfoProvider appInfo = Provider.of<AppInfoProvider>(context);
+    
+    return await showMenu(
+      context: context,
+      position: RelativeRect.fromLTRB(
+        appInfo.position.dx,
+        appInfo.position.dy,
+        appInfo.position.dx,
+        appInfo.position.dy,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      items: [
+        PopupMenuItem(
+          child: Row(
+            children: [
+              Icon(
+                CommunityMaterialIcons.check,
+                color: Theme.of(context).accentColor,
+              ),
+              SizedBox(width: 24),
+              Text("Select"),
+            ],
+          ),
+          value: 'select',
+        ),
+        PopupMenuItem(
+          child: Row(
+            children: [
+              Icon(
+                CommunityMaterialIcons.pin_outline,
+                color: Theme.of(context).accentColor,
+              ),
+              SizedBox(width: 24),
+              Text("Pin to notifications"),
+            ],
+          ),
+          enabled: !note.hideContent,
+          value: 'pin',
+        ),
+        PopupMenuItem(
+          child: Row(
+            children: [
+              Icon(
+                CommunityMaterialIcons.share_variant,
+                color: Theme.of(context).accentColor,
+              ),
+              SizedBox(width: 24),
+              Text("Share"),
+            ],
+          ),
+          enabled: !note.hideContent,
+          value: 'share',
+        ),
+      ],
     );
   }
 }
--- lib/routes/main_page.dart
+++ lib/routes/main_page.dart
@@ -312,60 +312,7 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
         onLongPress: () async {
           if (selecting) return;
 
-          String action = await showMenu(
-            context: context,
-            position: RelativeRect.fromLTRB(
-              appInfo.position.dx,
-              appInfo.position.dy,
-              appInfo.position.dx,
-              appInfo.position.dy,
-            ),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-            items: [
-              PopupMenuItem(
-                child: Row(
-                  children: [
-                    Icon(
-                      CommunityMaterialIcons.check,
-                      color: Theme.of(context).accentColor,
-                    ),
-                    SizedBox(width: 24),
-                    Text("Select"),
-                  ],
-                ),
-                value: 'select',
-              ),
-              PopupMenuItem(
-                child: Row(
-                  children: [
-                    Icon(
-                      CommunityMaterialIcons.pin_outline,
-                      color: Theme.of(context).accentColor,
-                    ),
-                    SizedBox(width: 24),
-                    Text("Pin to notifications"),
-                  ],
-                ),
-                enabled: !note.hideContent,
-                value: 'pin',
-              ),
-              PopupMenuItem(
-                child: Row(
-                  children: [
-                    Icon(
-                      CommunityMaterialIcons.share_variant,
-                      color: Theme.of(context).accentColor,
-                    ),
-                    SizedBox(width: 24),
-                    Text("Share"),
-                  ],
-                ),
-                enabled: !note.hideContent,
-                value: 'share',
-              ),
-            ],
-          );
+          String action = await Utils.showNoteMenu(context, note);
 
           if (action != null) {
             switch (action) {
