cb3978d744e4d0dee603ae7b335ff0994dfd099d
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2020-06-06 19:18:29 +0200

    notes: Use the notehelper from AppDatabase in locator instead of constructor
    As suggested by Hrx03 in his review of PR #35

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,4 +1,3 @@
-
 import 'package:get_it/get_it.dart';
 import 'package:potato_notes/data/dao/note_helper.dart';
 import 'package:potato_notes/internal/app_info.dart';
@@ -8,8 +7,7 @@ import 'main.dart';
 
 final locator = GetIt.instance;
 
-void setupLocator(){
+void setupLocator() {
   locator.registerSingleton<Preferences>(Preferences());
-  locator.registerLazySingleton<NoteHelper>(() => NoteHelper(db));
+  locator.registerLazySingleton<NoteHelper>(() => db.noteHelper);
 }
-
