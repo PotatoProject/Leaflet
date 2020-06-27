860555e039fea28109c0086876eca58f5387a908
HrX03 <dn.bianco03@gmail.com>
2020-06-27 10:06:23 +0200

    notes: Finally fix search

--- lib/data/dao/note_helper.dart
+++ lib/data/dao/note_helper.dart
@@ -68,68 +68,6 @@ class NoteHelper extends DatabaseAccessor<AppDatabase> with _$NoteHelperMixin {
         .watch();
   }
 
-  Future<List<Note>> getNotesMatchingQuery(SearchQuery query) async {
-    Expression<bool> dateModeBoolExpression($NotesTable table) {
-      Expression<bool> exp;
-
-      switch (query.dateMode) {
-        case DateFilterMode.AFTER:
-          exp = table.creationDate.isBiggerOrEqualValue(query.date);
-          break;
-        case DateFilterMode.BEFORE:
-          exp = table.creationDate.isSmallerOrEqualValue(query.date);
-          break;
-        case DateFilterMode.ONLY:
-        default:
-          exp = table.creationDate.equals(query.date);
-          break;
-      }
-
-      return exp;
-    }
-
-    SimpleSelectStatement<$NotesTable, Note> selectQuery;
-    List<Note> noteList;
-    List<Note> queryNotes = [];
-
-    if (query.color != null && query.date != null) {
-      selectQuery = select(notes)
-        ..where((table) =>
-            table.color.equals(query.color) & dateModeBoolExpression(table));
-    } else if (query.color != null) {
-      selectQuery = select(notes)
-        ..where((table) => table.color.equals(query.color));
-    } else if (query.date != null) {
-      selectQuery = select(notes)
-        ..where((table) => dateModeBoolExpression(table));
-    } else {
-      selectQuery = select(notes);
-    }
-
-    noteList = await selectQuery.get();
-
-    String textQuery =
-        query.caseSensitive ? query.input : query.input.toLowerCase();
-
-    if (textQuery.trim() != "") {
-      for (Note note in noteList) {
-        List<String> splittedTitle = query.caseSensitive
-            ? note.title.split(new RegExp("[ ]{1,}"))
-            : note.title.toLowerCase().split(new RegExp("[ ]{1,}"));
-        List<String> splittedContent = query.caseSensitive
-            ? note.content.split(new RegExp("[ ]{1,}"))
-            : note.content.toLowerCase().split(new RegExp("[ ]{1,}"));
-
-        if (splittedTitle.any((element) => element.startsWith(textQuery)) ||
-            splittedContent.any((element) => element.startsWith(textQuery))) {
-          queryNotes.add(note);
-        }
-      }
-    }
-
-    return queryNotes;
-  }
-
   Future saveNote(Note note) =>
       into(notes).insert(note, mode: InsertMode.replace);
 
--- lib/routes/search_page.dart
+++ lib/routes/search_page.dart
@@ -17,6 +17,7 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   NoteHelper helper;
   List<Note> notes = [];
+  int numOfImages;
 
   SearchQuery query = SearchQuery();
 
@@ -24,61 +25,59 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     if (helper == null) helper = locator<NoteHelper>();
 
+    double width = MediaQuery.of(context).size.width;
+    numOfImages = 2;
+
+    if (width >= 1280) {
+      numOfImages = 4;
+    } else if (width >= 900) {
+      numOfImages = 3;
+    } else if (width >= 600) {
+      numOfImages = 3;
+    } else if (width >= 360) {
+      numOfImages = 2;
+    } else {
+      numOfImages = 2;
+    }
+
     return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0),
+          child: TextField(
+            decoration: InputDecoration.collapsed(
+              hintText: "Search",
+            ),
+            onChanged: (text) {
+              query.input = text;
+              updateResults();
+            },
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.filter_list),
+            padding: EdgeInsets.all(0),
+            onPressed: () => showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) => QueryFilters(
+                query: query,
+                filterChangedCallback: () => updateResults(),
+              ),
+            ),
+          ),
+        ],
+      ),
       body: ListView.builder(
         itemBuilder: (context, index) {
           return NoteView(
             note: notes[index],
             onTap: () => openNote(notes[index]),
-            numOfImages: 0,
+            numOfImages: numOfImages,
           );
         },
         itemCount: notes.length,
-      ),
-      bottomNavigationBar: Material(
-        elevation: 6,
-        color: Theme.of(context).cardColor,
-        child: Container(
-          height: 56,
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          margin:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.arrow_back),
-                padding: EdgeInsets.all(0),
-                onPressed: () => Navigator.pop(context),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: TextField(
-                    decoration: InputDecoration.collapsed(
-                      hintText: "Search",
-                    ),
-                    onChanged: (text) {
-                      query.input = text;
-                      updateResults();
-                    },
-                  ),
-                ),
-              ),
-              IconButton(
-                icon: Icon(Icons.filter_list),
-                padding: EdgeInsets.all(0),
-                onPressed: () => showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  builder: (context) => QueryFilters(
-                    query: query,
-                    filterChangedCallback: () => updateResults(),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
@@ -86,8 +85,9 @@ class _SearchPageState extends State<SearchPage> {
   void updateResults() async {
     notes.clear();
     notes = List.from(
-      await helper.getNotesMatchingQuery(query),
+      getNotesForQuery(await helper.listNotes(ReturnMode.ALL)),
     );
+    setState(() {});
   }
 
   void openNote(Note note) async {
@@ -112,21 +112,6 @@ class _SearchPageState extends State<SearchPage> {
     }
 
     if (status) {
-      double width = MediaQuery.of(context).size.width;
-      int numOfImages = 2;
-
-      if (width >= 1280) {
-        numOfImages = 4;
-      } else if (width >= 900) {
-        numOfImages = 3;
-      } else if (width >= 600) {
-        numOfImages = 3;
-      } else if (width >= 360) {
-        numOfImages = 2;
-      } else {
-        numOfImages = 2;
-      }
-
       Navigator.push(
         context,
         MaterialPageRoute(
@@ -137,5 +122,78 @@ class _SearchPageState extends State<SearchPage> {
         ),
       );
     }
+  }
+
+  List<Note> getNotesForQuery(List<Note> notes) {
+    List<Note> results = [];
+
+    if(query.input.trim().isEmpty) {
+      return [];
+    }
+
+    bool _getColorBool(int noteColor) {
+      return noteColor == query.color;
+    }
+
+    bool _getDateBool(DateTime noteDate) {
+      DateTime sanitizedNoteDate = DateTime(
+        noteDate.year,
+        noteDate.month,
+        noteDate.day,
+      );
+
+      DateTime sanitizedQueryDate = DateTime(
+        query.date.year,
+        query.date.month,
+        query.date.day,
+      );
+
+      switch (query.dateMode) {
+        case DateFilterMode.AFTER:
+          return sanitizedNoteDate.isAfter(sanitizedQueryDate);
+        case DateFilterMode.BEFORE:
+          return sanitizedNoteDate.isBefore(sanitizedQueryDate);
+        case DateFilterMode.ONLY:
+        default:
+          return sanitizedNoteDate.isAtSameMomentAs(sanitizedQueryDate);
+      }
+    }
+
+    bool _getTextBool(String text) {
+      String sanitizedQuery =
+          query.caseSensitive ? query.input : query.input.toLowerCase();
+
+      String sanitizedText = query.caseSensitive ? text : text.toLowerCase();
+
+      return sanitizedText.contains(sanitizedQuery);
+    }
+
+    for (Note note in notes) {
+      if (query.color != null && query.date != null) {
+        if (_getColorBool(note.color) &&
+            _getDateBool(note.creationDate) &&
+            (_getTextBool(note.title) || _getTextBool(note.content))) {
+          results.add(note);
+        }
+      } else {
+        if (query.color != null) {
+          if (_getColorBool(note.color) &&
+              (_getTextBool(note.title) || _getTextBool(note.content))) {
+            results.add(note);
+          }
+        } else if (query.date != null) {
+          if (_getDateBool(note.creationDate) &&
+              (_getTextBool(note.title) || _getTextBool(note.content))) {
+            results.add(note);
+          }
+        } else {
+          if (_getTextBool(note.title) || _getTextBool(note.content)) {
+            results.add(note);
+          }
+        }
+      }
+    }
+
+    return results;
   }
 }
