e420414cfffb69495b37ff222180ab541b2cb658
Akshit Garg <garg.akshit@gmail.com>
2020-08-10 20:16:59 +0530

    password_encryption: add encryptTextToBinary and decryptBinaryToText

--- lib/internal/password_encryption.dart
+++ lib/internal/password_encryption.dart
@@ -4,6 +4,8 @@ import 'dart:typed_data';
 import 'package:cryptography/cryptography.dart';
 import 'package:encrypt/encrypt.dart';
 
+/// PasswordEncryption
+/// @author Akshit Garg <garg.akshit@gmail.com>
 class PasswordEncryption {
   static Future<String> encryptText(
     String plainText,
@@ -17,6 +19,18 @@ class PasswordEncryption {
     return '${encrypted.base64}|${iv.base64}';
   }
 
+  static Future<Uint8List> encryptTextToBinary(
+    String plainText,
+    String password,
+  ) async {
+    final key = Key(await deriveKey(password));
+    final encrypter = Encrypter(AES(key));
+    final iv = IV.fromSecureRandom(16);
+    final encrypted = encrypter.encrypt(plainText, iv: iv);
+
+    return Uint8List.fromList([...encrypted.bytes, ...iv.bytes]);
+  }
+
   static Future<Uint8List> encryptBinary(
     Uint8List data,
     String password,
@@ -26,9 +40,7 @@ class PasswordEncryption {
     final iv = IV.fromSecureRandom(16);
     final encrypted = encrypter.encrypt(base64.encode(data), iv: iv);
 
-    final finalData = Uint8List.fromList([...encrypted.bytes, ...iv.bytes]);
-
-    return finalData;
+    return Uint8List.fromList([...encrypted.bytes, ...iv.bytes]);
   }
 
   static Future<String> decryptText(
@@ -44,6 +56,25 @@ class PasswordEncryption {
       final decrypted = encrypter.decrypt(
         Encrypted.from64(encrypted[0]),
         iv: IV.fromBase64(encrypted[1]),
+      );
+
+      return decrypted;
+    } catch (e) {
+      throw Exception('There was an error decrypting the inputs');
+    }
+  }
+
+  static Future<String> decryptBinaryToText(
+    Uint8List data,
+    String password,
+  ) async {
+    final key = Key(await deriveKey(password));
+    final encrypter = Encrypter(AES(key));
+
+    try {
+      final decrypted = encrypter.decrypt(
+        Encrypted(data.sublist(0, data.length - 16)),
+        iv: IV(data.sublist(data.length - 16, data.length)),
       );
 
       return decrypted;
