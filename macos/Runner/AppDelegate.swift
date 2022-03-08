a2db87c141ff3364933fac6399e91bee49c9bb09
Akshit Garg <garg.akshit@gmail.com>
2022-03-08 22:32:06 +0530

    notes: add rust library support for macOS
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,8 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+  
+  func dummy() {
+    dummy_method_to_enforce_bundling()
+  }
 }
